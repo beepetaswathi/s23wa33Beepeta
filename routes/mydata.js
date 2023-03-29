a14efe8fd991d6bd7811d85151cfaa48f500c919
beepetaswathi <123126383+beepetaswathi@users.noreply.github.com>
2023-03-28 21:47:38 -0500

    file

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'swathi beepeta' });
+    res.render('mydata', { title: 'Swathi Beepeta' });
 });
 
 module.exports = router;
