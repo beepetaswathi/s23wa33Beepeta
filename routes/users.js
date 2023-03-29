db00a54c15f17c8f04d7826399e1191ffd891a13
beepetaswathi <123126383+beepetaswathi@users.noreply.github.com>
2023-03-29 14:31:18 -0500

    modified

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+var sum = 0;
+
+var more = 0;
+
+router.get('/', function(req, res, next) {
+
+more++;
+
+sum += more;
+
+res.send(`sum is :${sum}`);
+
+});
+
+module.exports = router;
